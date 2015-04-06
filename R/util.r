72f2e46f66cea89207196b337f3dfa29f7bcdca5
drknexus <Russell.s.pierce@gmail.com>
2015-04-06 00:19:01 -0400

    Update util.r

--- R/util.r
+++ R/util.r
@@ -1,8 +1,13 @@
 #' Catch errors and warnings and store them for subsequent evaluation
 #'
-#' Factory is not my code.  Factory generates a function which is appropriately wrapped by error handlers.  The result produced is a list.
-#' This is a nice way to recover from a loop problems that may have occured during loop evaluation.  Check the references for additional related functions.
-#' I have not included the other factory functions because they did not play well with the return item as an S4 object.
+#' Factory modified from a version written by Martin Morgan on Stack Overflow (see below).  
+#' Factory generates a function which is appropriately wrapped by error handlers.  
+#' If there are no errors and no warnings, the result is provided.  
+#' If there are warnings but no errors, the result is provided with a warn attribute set.
+#' If there are errors, the result retutrns is a list with the elements of warn and err.
+#' This is a nice way to recover from a loop problems that may have occured during loop evaluation or during cluster usage.
+#' Check the references for additional related functions.
+#' I have not included the other factory functions included in the original Stack Overflow answer because they did not play well with the return item as an S4 object.
 #' @export
 #' @param fun The function to be turned into a factory
 #' @return list[3]: item 1 is the result from the function and $warn and $err contain warning and error messages respectively.
@@ -14,17 +19,26 @@
 #' f.log("a")
 #' f.as.numeric <- factory(as.numeric)
 #' f.as.numeric(c("a","b",1))
-factory <- function(fun)
+factory <- function (fun) {
+  library(data.table)
   function(...) {
     warn <- err <- NULL
-    res <- withCallingHandlers(
-      tryCatch(fun(...), error=function(e) {
-        err <<- conditionMessage(e)
-        NULL
-      }), warning=function(w) {
-        warn <<- append(warn, conditionMessage(w))
-        invokeRestart("muffleWarning")
-      })
-    return(list(res, warn=warn, err=err))
+    res <- withCallingHandlers(tryCatch(fun(...), error = function(e) {
+      err <<- conditionMessage(e)
+      NULL
+    },finally=data.table::setattr(res,"factoryError",TRUE)), warning = function(w) {
+      warn <<- append(warn, conditionMessage(w))
+      invokeRestart("muffleWarning")
+    })
+    attrFactoryError <- attr(res,"factoryError")
+    if (is.null(attrFactoryError) & is.null(warn) & is.null(err)) {
+      return(res)
+    } else if (is.null(attrFactoryError) & is.null(err)) {
+      data.table::setattr(res,"warn",warn)
+      return(res)
+    } else {
+      return(list(warn = warn, err = err))
+    }
   }
+}
 NULL
